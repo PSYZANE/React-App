0dcd6a451a76f4809da69d711f8e4064dfc1bde6
Aditya-Budakoti <91477271+PSYZANE@users.noreply.github.com>
2025-05-07 23:09:44 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ const Navbar = () => {
   const [Menu, setMenu] = useState(false);
 
   const toggleMenu = () => {
-    setMenu(-Menu)
+    setMenu(!Menu)
   }
 
   return (
     <div className='Nav' class="bg-cyan-400 p-4 w-full flex flex-row">
         <p className="logo w-full text-2xl font-bold">PSYZANE</p>
-        <div className="tabs flex flex-row mr-8 md:gap-8 text-sm m-auto font-medium">
+        <div className="sm:hidden md:flex flex-row mr-8 gap-8 text-sm m-auto font-medium">
             <a className="nav_btn1 data-focus:bg-gray-100 data-focus:outline-hidden">one</a>
             <a className="nav_btn2 data-focus:bg-gray-100">two</a>
             <a className="nav_btn3 data-focus:bg-gray-100">three</a>
