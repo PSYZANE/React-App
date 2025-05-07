c30f057357d68612092d98d221c064f84310c8f4
Aditya-Budakoti <91477271+PSYZANE@users.noreply.github.com>
2025-05-07 23:59:53 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='h-4/5 bg-blue-700 ml-auto'>
-        <h1 className="text-9xl font-bold">Hero</h1>
+    <div className='h-lvh bg-blue-700 ml-auto relative'>
+        <h1 className="absolute left-10/12 top-1/12  text-9xl font-bold [writing-mode:vertical-lr]">HERO</h1>
+        <h1 className="absolute left-1/12 top-6/12 text-9xl font-bold [writing-mode:vertical-rl] rotate-180 text-red-600 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">HERO</h1>
+
+        <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-8/12 text-[20vw] font-bold">
+          HERO
+          <div className='relative'>
+            <h1 className="absolute top-1/2 -translate-y-11/12 text-[20vw] font-bold opacity-95">HERO</h1>
+            <h1 className="absolute top-1/2 -translate-y-10/12 text-[20vw] font-bold opacity-90">HERO</h1>
+            <h1 className="absolute top-1/2 translate-y-11/12 text-[20vw] font-bold">HERO</h1>
+            <h1 className="absolute top-1/2 translate-y-10/12 text-[20vw] font-bold">HERO</h1>
+          </div>
+        </h1>
+
+        
     </div>
   )
 }
